74619a91bf601f2df923f058184a849d6fa2fb68
0942650532 <mathamolina21072001@gmail.com>
2024-05-16 19:16:53 -0500

    agregamos appbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MainApp());
 }
-
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
+        appBar: AppBar(
+         title: const Text('My App'),
+        ),
+        body: const Center(
           child: Text('Hello World!'),
         ),
-      ),
-    );
-  }
+     ),
+);
+}
 }
